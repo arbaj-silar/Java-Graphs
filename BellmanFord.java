532f3ea63971795fa48eedfcbfe826a06c4d818a
Arbaj <silararbaj@gmail.com>
2026-04-12 06:26:47 +0530

    Negative Weights

--- BellmanFord.java
+++ BellmanFord.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.PriorityQueue;
 
 public class BellmanFord {
     static class Edge {
@@ -49,20 +48,18 @@ public class BellmanFord {
                     }
                 }
             }
-
-            for(int i=0; i<V; i++) {
-                for(int j=0; j<graph[i].size(); j++) {
-                    Edge e = graph[i].get(j);
-                    int u = e.src;
-                    int v = e.dest;
-    
-                    if(dist[u] != Integer.MAX_VALUE && dist[u]+e.wt < dist[v]) {
-                        System.out.println("Negative weight Infinte cycle");
-                    }
+        }
+        
+        for(int i=0; i<V; i++) {
+            for(int j=0; j<graph[i].size(); j++) {
+                Edge e = graph[i].get(j);
+                int u = e.src;
+                int v = e.dest;
+                if(dist[u] != Integer.MAX_VALUE && dist[u]+e.wt < dist[v]) {
+                    System.out.println("Negative weight Infinte cycle");
                 }
             }
         }
-
         for(int i=0; i<dist.length; i++) {
             System.out.print(dist[i] + " ");
         }
